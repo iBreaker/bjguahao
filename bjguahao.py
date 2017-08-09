9260b8a97f31715cce6360b9d6311603efd54431
Nikolas Yuan <rabbit1989725@gmail.com>
2017-08-09 17:25:48 +0800

    获取病人id的逻辑位置改变为选择医生之后马上获取病人ID

--- bjguahao.py
+++ bjguahao.py
@@ -321,6 +321,7 @@ class Guahao(object):
         doctor = "";
         while True:
             doctor = self.select_doctor()            # 2. 选择医生
+            self.get_patient_id(doctor)              # 3. 获取病人id
             if doctor == "NoDuty":
                 Log.error("没号了,  亲~")
                 break
@@ -332,7 +333,6 @@ class Guahao(object):
                 if sms_code == None:
                     time.sleep(1)
 
-                self.get_patient_id(doctor)         # 3. 获取病人id
                 result = self.get_it(doctor, sms_code)                 # 4.挂号
                 if result == True:
                     break                                    # 挂号成功
