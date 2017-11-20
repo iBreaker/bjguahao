5ef0757edb0f731a9e00cdb8f2ef7e7bacd5c581
Breaker <791628659@qq.com>
2017-11-20 16:52:09 +0800

    修复bug

--- bjguahao.py
+++ bjguahao.py
@@ -216,6 +216,8 @@ class Guahao(object):
 
     def get_patient_id(self, doctor):
         """获取就诊人Id"""
+        if isinstance(doctor, str):
+            self.logger.exit("没号了,  亲~")
         addr = self.gen_doctor_url(doctor)
         response = self.browser.get(addr, "")
         ret = response.text
