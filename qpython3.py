af86817b1c96711ffcb714ae24be2429478935ad
CoeusITE <coeusite@gmail.com>
2018-04-20 09:49:16 +0800

    自动监测调用时间节点后的短信

--- qpython3.py
+++ qpython3.py
@@ -1,6 +1,7 @@
 import re
 import datetime
 import logging
+import time
 
 from androidhelper import Android
 
@@ -13,7 +14,6 @@ class QPython3(object):
         # Android QPython3
         self.droid = Android()
         logging.debug("QPython3 实例初始化完成")
-
     # 读取验证码短信
     def _get_sms_verify_code(self):
         # init
@@ -21,47 +21,59 @@ class QPython3(object):
         code = '000000'
         retry = 600
         # loop
+        logging.debug("监控短信中……")
         while retry > 0:
             retry -= 1
             # 检查 SMS
             code = self._check_sms_verify_code()
             # 有效验证码？
             if code != '000000':
+                logging.debug("取得有效验证码……"+code)
                 break
             else:
+                logging.debug("未找到有效验证码……重试中, retry = {}".format(retry))
                 time.sleep(0.05)
+        else:
+            logging.debug("未找到有效验证码……"+code)
         return code
-
     # 读取验证码短信
     def _check_sms_verify_code(self):
         # init
         code = '000000'
         # 获取当前的全部未读短信
         smsMessageIds = self.droid.smsGetMessageIds(True)
+        # 无短信退出
+        if len(smsMessageIds.result) == 0:
+            logging.debug("无短信退出……")
+            return code
         # loop
-        for smsId in smsMessageIds:
+        for smsId in smsMessageIds.result:
             # get message
             smsMessage = self.droid.smsGetMessageById(smsId)
-            # TODO 时间筛选
-            smsTimestamp = smsMessage.timestamp
+            # 时间筛选
+            smsTimestamp = datetime.datetime.fromtimestamp(int(smsMessage.result['date'])/1e3)
             # 跳过开始时间点前 SMS
             if smsTimestamp < self.start_time:
+                # print("时间跳过:", smsMessage)
                 continue
-            # TODO 文字匹配
-            smsContent = smsMessage.content
+            # 文字匹配
+            smsContent = smsMessage.result['body']
             res = self.regex.search(smsContent)
             if res is None:
+                # print("匹配跳过:", smsMessage)
                 continue
             else:
+                # print("发现短信:", smsMessage)
                 code = res.group(1).strip()
                 break
         return code
-
     # 获取验证码的外部调用
     def get_verify_code(self):
         logging.debug("获取验证码中……")
         # 读取验证码短信
         return self._get_sms_verify_code()
+
+
 
 ## 测试使用
 def main():
